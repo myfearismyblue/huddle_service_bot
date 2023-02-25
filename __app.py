53143cb1262e88770e001a7f31033342c3cd24ad
Andrey Smekhov <myfearismyblue@gmail.com>
2023-02-25 18:02:26 +0200

    Add inline query articles for milongas and oldclothers

--- __app.py
+++ __app.py
@@ -4,7 +4,7 @@ import logging
 import sys
 sys.path.append('../venv/lib/python3.8/site-packages')
 
-from vk_services.services import GroupDomainNameAliases, VKHandler
+from vk_services.services import GroupDomainNameAliases, VKHandler, VKGroupGrabber
 
 from aiogram import Bot, Dispatcher, executor, types
 
@@ -25,12 +25,19 @@ if __name__ == '__main__':
 
     @dp.inline_handler()
     async def custom_handler(query):
-        text = 'Inline greetings'
-        r_sum = types.InlineQueryResultArticle(
-            id='First id', title="Hint",
-            description="Description",
-            input_message_content=types.InputTextMessageContent(message_text=text))
-        await bot.answer_inline_query(query.id, [r_sum])
+        milonga_query = types.InlineQueryResultArticle(
+                        id='First id', title="Milongas",
+                        description="Last posted polling",
+                        thumb_url='https://sun9-west.userapi.com/sun9-67/s/v1/if2/FlPZoG_0w6Lwt8qg6C68xOSHUG7WFtmtRLUO_QLlWW6XNBLrOT-vYRy_2SUnj5Sk4H2dpUzNTcqSqpYGOflh0i8k.jpg?size=600x600&quality=96&type=album',
+                        input_message_content=types.InputTextMessageContent(message_text='/milonga'))
+
+        oldclothers_query = types.InlineQueryResultArticle(
+                            id='Second id', title="Отдам даром",
+                            description="Последний пост группы \"Отдам даром \"",
+                            thumb_url='https://sun9-west.userapi.com/sun9-13/s/v1/if1/_CFTXZftew3F_o2qb8_6jSxhETtO7-hbBYuWLQMDqpPtL_30nGxRL_MH4uniWsBlI_gulCHZ.jpg?size=1065x1032&quality=96&type=album',
+                            input_message_content=types.InputTextMessageContent(message_text='/old'))
+
+        await bot.answer_inline_query(query.id, [milonga_query, oldclothers_query])
 
     async def on_startup(_):
         logging.warning('Bot is running')
